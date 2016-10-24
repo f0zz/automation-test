cd5a1fc06eff5f3fc3d73330e9ad68c4f36bfa13
fozz <mlgiardina3@gmail.com>
2016-10-24 11:17:00 -0500

    trying successful test

--- groovy-template.groovy
+++ groovy-template.groovy
@@ -42,7 +42,7 @@ TD.console { font-family:Courier New; }
   <TR><TD>Job duration:</TD><TD>${build.durationString}</TD></TR>
 </TABLE>
 <BR/>
-<p><img src="http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=fail&rating=g"/></p>
+<p><img src="http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=fail&rating=g&fmt=html"/></p>
 <%
 def robotResults = false
 def actions = build.actions // List<hudson.model.Action>
@@ -80,43 +80,17 @@ actions.each() { action ->
 <tr class="headerRow">
   <td><b>Test Name</b></td>
   <td><b>Status</b></td>
-  <td><b>Execution Datetime</b></td>
 </tr>
 </thead>
 <tbody>
-<%  def suites = action.result.allSuites
-    suites.each() { suite ->
-      def currSuite = suite
-      def suiteName = currSuite.displayName
-      // ignore top 2 elements in the structure as they are placeholders
-      while (currSuite.parent != null && currSuite.parent.parent != null) {
-        currSuite = currSuite.parent
-        suiteName = currSuite.displayName + "." + suiteName
-      } %>
-      <%if (allPassed(suite.caseResults)) { %>
-        <tr><td colspan="3"><b><%= suiteName %></b></td></tr>
-        <%    DateFormat format = new SimpleDateFormat("yyyyMMdd HH:mm:ss.SS")
-              def execDateTcPairs = []
-              suite.caseResults.each() { tc ->
-                Date execDate = format.parse(tc.starttime)
-                execDateTcPairs << [execDate, tc]
-              }
-              // primary sort execDate, secondary displayName
-              execDateTcPairs = execDateTcPairs.sort{ a,b -> a[1].displayName <=> b[1].displayName }
-              execDateTcPairs = execDateTcPairs.sort{ a,b -> a[0] <=> b[0] }
-              execDateTcPairs.each() {
-                def execDate = it[0]
-                def tc = it[1]  %>
-                <% if (!tc.isPassed()) { %>
-                    <tr>
-                      <td><%= tc.displayName %></td>
-                      <td style="color: #FF3333">FAIL</td>
-                      <td><%= execDate %></td>
-                    </tr>
-        <%      }
-              } // tests
-      }
-    } // suites %>
+<% def failedResults = action.getResult().getAllFailedCases()
+    failedResults.each { test ->
+        def currTest = test %>
+        <tr>
+          <td><%= currTest.displayName %></td>
+          <td style="color: #FF3333">FAIL</td>
+        </tr>
+    <%}%>
 </tbody></table>
 <p><a href="${rooturl}${build.url}robot-plugin/report.html">Detailed Report</a></p>
 <%
